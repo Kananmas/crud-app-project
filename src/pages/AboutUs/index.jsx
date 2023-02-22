9cc65f9725bf7c1ac1ab798ccd805218e9d77330
kanan <kananmasajedi@gmail.com>
2023-02-22 20:26:23 +0330

    v.1.0.7

--- src/pages/AboutUs/index.jsx
+++ src/pages/AboutUs/index.jsx
@@ -1,39 +1,93 @@
-import { IonContent, IonText } from "@ionic/react";
-import { StyledIonText } from "../PerviousRecords/index.styled";
+import { IonButton, IonContent, IonText } from "@ionic/react";
+import { useHistory } from "react-router";
 import {
   StyledHolder,
   StyledContainer,
   StyledTextHolder,
+  StyledTitle,
+  StyledLinkedInIcon,
+  LinksText,
+  ButtonHolder,
 } from "./index.styled";
 
 export function AboutUs() {
+  const History = useHistory();
+
+  function HandleOnClickBack() {
+    History.go(-1);
+  }
+
   return (
     <IonContent>
       <StyledHolder>
         <StyledContainer>
           <h4>About Misspelle</h4>
           <h6>A-TEAM</h6>
+          <h6>V.1.0.6</h6>
           <br />{" "}
           <IonText>
             This project is done with the efforts of all memebers of the A-team
-            and we are all greatful to them for their passion and Their working
+            and we are all greatful to them for their passion and working
             etheics, in this project:
           </IonText>
         </StyledContainer>
         <StyledTextHolder>
-          <br /> <h3>Kanan Masajedi :</h3> Leader of project and responsible for
-          the logic of project <br /> <h3> Hamid Shokatinia:</h3> responsible
-          for redux-saga and unit testing <br /> <h3> Zahra Mohseni:</h3>
+          <br />{" "}
+          <StyledTitle>
+            {" "}
+            <LinksText href="https://www.linkedin.com/in/kanan-mas-a75925254/">
+              Kanan Masajedi
+            </LinksText>
+            <StyledLinkedInIcon />
+          </StyledTitle>{" "}
+          Leader of project and responsible for the logic of project <br />{" "}
+          <StyledTitle>
+            {" "}
+            <LinksText href="https://www.linkedin.com/in/hamid-shokatinia">
+              Hamid Shokatinia
+            </LinksText>
+            <StyledLinkedInIcon />
+          </StyledTitle>{" "}
+          responsible for redux-saga and unit testing <br />{" "}
+          <StyledTitle>
+            {" "}
+            <LinksText>Zahra Mohseni</LinksText>
+            <StyledLinkedInIcon />
+          </StyledTitle>
           responsible for creating the UI and UX and writing end to end tests
           for project
           <br />
-          <h3> Somaye Baniasadi:</h3> responsible for UI and UX creation of the
-          Previous Records page and question page
-          <br /> <h3>Mahdi Fallahi :</h3> responsible for UI and UX design of
-          the result and past result page and creating pie chart
+          <StyledTitle>
+            {" "}
+            <LinksText>Somaye Baniasadi</LinksText>
+            <StyledLinkedInIcon />
+          </StyledTitle>{" "}
+          responsible for UI and UX creation of the Previous Records page and
+          question page
           <br />
-          <h3> Mohammad Afshar:</h3> responsible for creating UI and UX of the
-          sign in and sign up page
+          <StyledTitle>
+            {" "}
+            <LinksText>Mahdi Fallahi</LinksText>
+            <StyledLinkedInIcon />
+          </StyledTitle>{" "}
+          responsible for UI and UX design of the result and past result page
+          and creating pie chart
+          <br />
+          <StyledTitle>
+            {" "}
+            <LinksText>Mohammad Afshar</LinksText>
+            <StyledLinkedInIcon />
+          </StyledTitle>{" "}
+          responsible for creating UI and UX of the sign in and sign up page
+          <ButtonHolder>
+            <IonButton
+              color="danger"
+              fill="outline"
+              onClick={HandleOnClickBack}
+            >
+              Back
+            </IonButton>
+          </ButtonHolder>
         </StyledTextHolder>
       </StyledHolder>
     </IonContent>
--- src/pages/AboutUs/index.styled.js
+++ src/pages/AboutUs/index.styled.js
@@ -1,16 +1,40 @@
 import styled from "styled-components";
-
+import { AiOutlineLinkedin } from "react-icons/ai";
 
 export const StyledHolder = styled.div`
-    padding:14%;
+    padding:14% 2%;
     font-size:18px;
 `
 
 export const StyledContainer = styled.div`
     font-size:18px;
-    border:2px solid red
+    border:2px solid red;
+    text-align:center;
+    padding:13px;
+    border-radius:14px;
 `
 
 export const StyledTextHolder = styled.div`
     text-align:left
+`
+export const StyledTitle = styled.h4`
+    background-color:#fc7300;
+    padding:14px;
+    border-radius:14px;
+`
+export const LinksText = styled.a`
+    color:white;
+    font-size:20px;
+    text-decoration:none
+`
+
+export const StyledLinkedInIcon = styled(AiOutlineLinkedin)`
+    font-size:20px;
+    color:#00425a;
+    margin-left:10px;
+`
+
+export const ButtonHolder = styled.div`
+text-align:center;
+margin-top:24px;
 `
